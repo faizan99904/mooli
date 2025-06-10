b63bcd1dfe094904e2dc35f9419a1eb2153ff1ce
faizan99904 <faizan99904@gmail.com>
2025-06-10 17:25:29 +0500

    add chart and working on map

--- src/app/modules/client/charts/column-chart/column-chart.component.ts
+++ src/app/modules/client/charts/column-chart/column-chart.component.ts
@@ -1,11 +1,106 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexPlotOptions,
+  ApexYAxis,
+  ApexLegend,
+  ApexStroke,
+  ApexXAxis,
+  ApexFill,
+  ApexTooltip,
+  NgApexchartsModule
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  yaxis: ApexYAxis;
+  xaxis: ApexXAxis;
+  fill: ApexFill;
+  tooltip: ApexTooltip;
+  stroke: ApexStroke;
+  legend: ApexLegend;
+};
 
 @Component({
   selector: 'app-column-chart',
-  imports: [],
   templateUrl: './column-chart.component.html',
-  styleUrl: './column-chart.component.scss'
+  styleUrls: ['./column-chart.component.scss'],
+  imports:[NgApexchartsModule]
 })
-export class ColumnChartComponent {
+export class ColumnChartComponent implements OnInit {
+  @ViewChild("chart") chart!: ChartComponent;
+  public chartOptions: any;
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "Net Profit",
+          data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+        },
+        {
+          name: "Revenue",
+          data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+        },
+        {
+          name: "Free Cash Flow",
+          data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+        }
+      ],
+      chart: {
+        type: "bar",
+        height: 300
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: "55%",
+          // endingShape: "rounded"
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        show: true,
+        width: 2,
+        colors: ["transparent"]
+      },
+      xaxis: {
+        categories: [
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+        ]
+      },
+      yaxis: {
+        title: {
+          text: "$ (thousands)"
+        }
+      },
+      fill: {
+        type: "fill",
+        colors: ["#17C2D7", "#9367B4", "#A5D8A2"],
+      },
+      // tooltip: {
+      //   y: {
+      //     formatter: function (val) {
+      //       return "$ " + val + " thousands";
+      //     }
+      //   }
+      // }
+    };
+  }
+
+  ngOnInit(): void {
+  }
 
 }
--- src/app/modules/client/worldmap/worldmap.component.ts
+++ src/app/modules/client/worldmap/worldmap.component.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-worldmap',
+  standalone:true,
   imports: [],
   templateUrl: './worldmap.component.html',
   styleUrl: './worldmap.component.scss'
 })
 export class WorldmapComponent {
+  zoom: number = 8;
+  lat: number = 51.673858;
+  long: number = 7.815982;
+  constructor() { }
 
+  ngOnInit(): void {
+  }
 }
