3d088b33249bcdd6dfe230e04773887f334c3b73
faizan99904 <faizan99904@gmail.com>
2025-06-22 12:04:31 +0500

    add edit user api

--- src/app/modules/client/User/users/users.component.ts
+++ src/app/modules/client/User/users/users.component.ts
@@ -7,23 +7,41 @@ import { Config } from 'datatables.net';
 import { Router, RouterLink } from '@angular/router';
 import { BackendService } from '../../../../core/services/backend.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule, DataTablesModule, RouterLink],
+  imports: [CommonModule, DataTablesModule, RouterLink, ReactiveFormsModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss',
 })
 export class UsersComponent implements OnInit {
   http = inject(HttpClient);
+  isEditUser:boolean = false
   @ViewChild(DataTableDirective, { static: false })
   dtDirective!: DataTableDirective;
+  userForm!: FormGroup;
+
   constructor(
     private backend: BackendService,
     private toast: ToastrService,
-    private router: Router
-  ) {}
+    private router: Router,
+    private fb:FormBuilder
+  ) {
+
+    this.userForm = this.fb.group({
+      userId:[''],
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mobile: ['', [Validators.required]],
+      address: [''],
+      status: ['', Validators.required]
+    });
+
+  }
 
   dtOptions: Config = {};
   Users: any[] = [];
@@ -73,7 +91,41 @@ export class UsersComponent implements OnInit {
     });
   }
 
+  renderTable(){
+    this.dtDirective.dtInstance.then((dtInstance:any)=>{
+     dtInstance.draw()
+    })
+  }
+
   editUser(user: any) {
     this.router.navigate(['/create-user'], { state: { user } });
   }
+
+  editUserModal(user:any){
+    this.isEditUser = !this.isEditUser
+    this.userForm.patchValue({
+      username: user.username,
+      firstName: user.lastName,
+      lastName: user.lastName,
+      email: user.email,
+      mobile: user.mobile,
+      address: user.address,
+      status: user.status,
+      userId: user._id
+    });
+  }
+
+  onSubmit(){
+     this.http.put(CONFIG.editUser, this.userForm.value).subscribe({
+         next:(res:any)=>{
+             this.toast.success("edit user successfully");
+             this.renderTable()
+         },
+         error:(error)=>{
+           this.toast.error("error");
+         }
+     })
+  }
+ 
+
 }
