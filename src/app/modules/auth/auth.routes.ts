ebaaf9dd02812776ab41179a6dc7a3065d639b7c
Maaz-Bukhari <maazbukhari1144@gmail.com>
2026-06-07 16:28:05 +0500

    changes

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -1,13 +1,12 @@
 import { Routes } from '@angular/router';
-import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 export const authRoutes: Routes = [
   {
     path: 'login',
-    component: LandingComponent,
-    data: { title: 'Mooli | Showcase' },
+    pathMatch: 'full',
+    redirectTo: 'login/access',
   },
   {
     path: 'login/access',
